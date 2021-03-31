19944c2aca3cfb9fe72503da5db72e31c5b4104c
PacosMartini <martinreye@yahoo.com>
2021-03-31 13:56:02 -0500

    Removes comments.

--- src/Components/ImageCard.js
+++ src/Components/ImageCard.js
@@ -1,10 +1,6 @@
 import React from 'react';
 
 class ImageCard extends React.Component{
-    // constructor(props){
-    //     super(props);
-    // }
-
     render() {
         return (
             <>
